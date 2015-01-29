a9f17c46e532f9f4983682409a45bedaf4be72ed
davecusatis1 <davecusatis1@gmail.com>
2015-01-29 01:07:18 -0500

    added mongodb for saving Spaces

--- main.py
+++ main.py
@@ -13,9 +13,14 @@ from tornado.concurrent import Future
 from tornado import gen
 from tornado.options import define, options, parse_command_line
 from tornado.httpserver import HTTPServer
+from pymongo import MongoClient
 
 define("port", default=8888, help="run on the given port", type=int)
 define("debug", default=False, help="run in debug mode")
+
+client = MongoClient('localhost', 27017)
+db = client.CustCollab
+collection = db['rooms']
 
 
 class MessageBuffer(object):
@@ -81,15 +86,21 @@ class LoginHandler(BaseHandler):
     #     self.redirect("/room/"+room_id, True)
 
 
+def get_room_id(referer):
+    room = re.search('(?<=room/)(.*)', referer)
+    if room is None:
+        room = "index"
+    else:
+        room = room.group(0)
+    return room
+
+
 class MessageNewHandler(BaseHandler):
     @tornado.web.authenticated
+    @gen.coroutine
     def post(self):
         #self._log()
-        room = re.search('(?<=room/)(.*)', self.request.headers.get("Referer"))
-        if room is None:
-            room = "index"
-        else:
-            room = room.group(0)
+        room = get_room_id(self.request.headers.get('Referer'))
         message = {
             "room": room,
             "id": str(uuid.uuid4()),
@@ -108,6 +119,18 @@ class MessageNewHandler(BaseHandler):
             self.write(message)
 
         room_buffers[message["room"]].new_messages([message])
+        room_buffers[message["room"]].wait_for_messages()
+        # update messages, write to DB
+        room_document = collection.find_one({"room_id": room})
+        if room_document is not None:
+            collection.update(
+                room_document,
+                {
+                    '$set': {'messages': room_buffers[room].cache}
+                },
+                upsert=False,
+                multi=False
+            )
 
 
 class MessageUpdatesHandler(BaseHandler):
@@ -143,15 +166,31 @@ class AuthLogoutHandler(BaseHandler):
 
 
 class RoomHandler(BaseHandler):
-    def prepare(self):
-        # todo load chat/editor/google hangs
-
-        return
+    # def prepare(self, room_id):
+    #     # todo load chat/editor/google hangs
+    #     # loads chat
+    #     room_buffers[room_id].cache = collection.find_one({'room_id': room_id}).messages
+    #     self.render("index.html", room_buffers[room_id].cache)
+    #
+    #     return
 
     @tornado.web.authenticated
     def get(self, room_id):
         #self.write(self.get_current_user())
-        self.render("index.html", messages=room_buffers[room_id].cache)
+        user = self.get_current_user()
+        room = collection.find_one({'room_id': room_id})
+
+        if room is not None:
+            room_buffers[room_id].cache = room['messages']
+
+        #if the user is not in the room, then add that son of a gun
+        if (room is not None) and (user not in room['users']):
+            collection.update(
+                room,
+                {'$addToSet': {'users': user}}
+            )
+
+        self.render("index.html", messages=room['messages'])
         return
 
 
@@ -160,6 +199,11 @@ class NewRoomHandler(BaseHandler):
         user = self.get_argument("name")
         room_id = self.get_argument("room_id")
         room_buffers[room_id] = MessageBuffer()
+        collection.insert({
+            'room_id': room_id,
+            'messages': room_buffers[room_id].cache,
+            'users': [user]
+        })
         self.redirect("/room/" + room_id)
 
 
@@ -184,9 +228,9 @@ def main():
         debug=options.debug
     )
     logging.getLogger().setLevel(logging.DEBUG)
-    #app.listen(options.port)
-    http_server = HTTPServer(app)
-    http_server.listen(options.port)
+    app.listen(options.port)
+    # http_server = HTTPServer(app)
+    # http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
